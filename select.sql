af0fb58fc865f38f74086a7569ccb17176a00a26
Павел Желнов <pzhelnov@p1m.org>
2019-10-23 12:23:48 +0300

    Начать обсчитывать базу
    Сюда загружены все записи по GSQ
    по спинальности КТ/МРТ/операция,
    выписанные с 2013-11-01 по 2019-08-23,
    если я ничего не путаю.
    
    Проверяем.
    
    Есть 4 CSV-файла, которые я подготовил
    24 августа непосредственно из XLS-экспортов:
    
    GSQ_Sx_CT_MRI_DCd_2013-11-01_thru_2017-12-31_Pt1.csv
    GSQ_Sx_CT_MRI_DCd_2013-11-01_thru_2017-12-31_Pt2.csv
    GSQ_Sx_CT_MRI_DCd_2018-01-01_thru_2018-09-01.csv
    GSQ_Sx_CT_MRI_DCd_2018-09-02_thru_2019-08-23.csv
    
    Первый экспорт пришлось разделить на две
    части, так как не загружался в БД.
    
    Процессу конвертации я доверяю, потому
    что внимательно делал. Остается проверить,
    не потерялось ли что на импорте в БД.
    
    Открываю Экселем (разделители — ";",
    так как и конвертировал в CSV тоже Экселем,
    но только в UTF-8 потом сохранял и строку
    заголовков менял на ту, что в БД — в общем,
    в будущем можно и это все оставлять;
    кстати, UTF-8 надо делать с BOM, а не то
    Excel for Mac 16.16.4 выдает кракозябры).
    
    Число строк вместе с заголовком:
    
    5956 + 6473 + 2413 + 4013 = 18855
    
    Минус четыре — равно 18851, как и в БД.
    
    Йу-ху-у!

--- select.sql
+++ select.sql
@@ -1,4 +1,33 @@
+/* Работа на 48 */
+
 SELECT
 if((spinality_type_manual_1 = "spinality-surgery" OR spinality_type_manual_1 = "spinality-ct-or-mri" OR spinality_type_manual_1 = "spinality-exam" OR spinality_type_manual_1 = "spinality-admission-dx") AND (spinality_type_manual_2 = "spinality-surgery" OR spinality_type_manual_2 = "spinality-ct-or-mri" OR spinality_type_manual_2 = "spinality-exam" OR spinality_type_manual_2 = "spinality-admission-dx"),2,if((spinality_type_manual_1 = "spinality-surgery" OR spinality_type_manual_1 = "spinality-ct-or-mri" OR spinality_type_manual_1 = "spinality-exam" OR spinality_type_manual_1 = "spinality-admission-dx") OR (spinality_type_manual_2 = "spinality-surgery" OR spinality_type_manual_2 = "spinality-ct-or-mri" OR spinality_type_manual_2 = "spinality-exam" OR spinality_type_manual_2 = "spinality-admission-dx"),1,0)) as "1-or-2-or-3-or-4-spinality",
 if(spinality_type_manual_1 = "spinality-not-admission-dx" && spinality_type_manual_2 = "spinality-not-admission-dx",2,if(spinality_type_manual_1 = "spinality-not-admission-dx" OR spinality_type_manual_2 = "spinality-not-admission-dx",1,0)) as "5-spinality-not-admission-dx"
 FROM `ft_form_7`
+
+/* Разные статистики по базе */
+
+SELECT count(*)
+FROM `gsq_sx_ct_mri_dcd`;
+
+/* count(*)
+18851 */
+
+select count(f1)
+from
+(SELECT count(*) f1
+FROM `gsq_sx_ct_mri_dcd`
+group by upi, mrn) t1;
+
+/* count(f1)
+9800 */
+
+select count(f1)
+from
+(SELECT count(*) f1
+FROM `gsq_sx_ct_mri_dcd`
+where spinality_type='Спинальная операция'
+group by upi, mrn) t1;
+
+/* count(f1)
+3468 */
