622bd541d24e6db59beac64f3206bc3bf06703eb
Павел Желнов <pzhelnov@p1m.org>
2021-02-23 12:37:56 +0300

    Отобразить в FT только с опросниками

--- 202102190513.sql
+++ 202102190513.sql
@@ -6,3 +6,14 @@ WHERE f1.`upat_sf36_trng` IS NOT NULL AND f2.uid REGEXP '^[0-9]+$';
 update `ft_form_12`
 set `has_proms_croms` = 1
 where `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****' OR `col_2` = '*****';
+
+/* displaying those with proms/croms only */
+
+update `ft_form_12`
+set `is_finalized` = null;
+
+update `ft_form_12`
+set `is_finalized` = 'yes'
+where `has_proms_croms` = 1;
+
+/* ok; showing 342 records */
